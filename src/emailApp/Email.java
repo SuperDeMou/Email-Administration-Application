2be40563ef4895fd90d46b447a7c3bd9721eb69b
Gustavo De Moura <136927780+SuperDeMou@users.noreply.github.com>
2023-09-04 14:28:04 -0300

    Removing comments

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -12,24 +12,18 @@ public class Email {
     private String companySuffix = "miscompany.com";
     private int mailBoxCapacity=500;
     private int defaultPasswordLength=10;
-
-    // Constructor to receive first name and last name
+    
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
 
-        // Call a method asking for the department - return the department
         this.department = setDepartment();
-        //System.out.println("Department: " + this.department);
 
-        // Call a method that return a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is: " + this.password);
 
-        // Combine elements to generate email
         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
     }
-    // Ask for the department
     private String setDepartment() {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("New worker: " + firstName + ". Department Codes:\n1 for Sale\n2 for Development\n3 for Accounting\n0 for none");
@@ -41,7 +35,6 @@ public class Email {
         }
     }
 
-    // Generate a random password
     private String randomPassword(int length){
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
         char[] password = new char[length];
@@ -52,17 +45,14 @@ public class Email {
         return new String(password);
     }
 
-    // Set the mailbox capacity
     public void setMailBoxCapacity(int capacity) {
         this.mailBoxCapacity = capacity;
     }
 
-    // Set the alternate email
     public void setAlternateEmail(String alternateEmail) {
         this.alternateEmail = alternateEmail;
     }
 
-    // Change the password
     public void changePassword(String password){
         this.password = password;
     }
